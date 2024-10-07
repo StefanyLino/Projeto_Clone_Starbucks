a25c14d525a36077f8c6d95eb4608305445589ee
Stefany Lino <stefanylinosilva47@gmail.com>
2024-10-07 16:19:28 -0300

    Mudar - JavaScript

--- mudar.js
+++ mudar.js
@@ -1,2 +1,13 @@
 // Linguagem de programação
 // Abre "" fecha ;
+
+// primeira parte Habilitado/Desabilidado
+function ToggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var links = document.querySelector('.links');
+    // Troca o estado da classe .toggle para ativo
+    menuToggle.classList.toggle('active')
+    // troca o estado da classe .links para ativo
+    links.classList.toggle('active')
+
+}
